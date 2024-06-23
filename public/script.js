849fb9cd25aa661b2b9c16f0425a5405bf7cbe22
akanksha8130 <akanksha20shukla@gmail.com>
2024-06-23 16:05:37 +0530

    new

--- public/script.js
+++ public/script.js
@@ -22,34 +22,11 @@ navigator.mediaDevices
         myStream = stream;
         addVideoStream(myVideo, stream);
 
-        socket.on("user-connected", (userId) => {
-            connectToNewUser(userId, stream);
-        });
+      
 
-        peer.on("call", (call) => {
-            call.answer(stream);
-            const video = document.createElement("video");
-            call.on("stream", (userVideoStream) => {
-                addVideoStream(video, userVideoStream);
-            });
-        });
+     
     })
 
-function connectToNewUser(userId, stream) {
-    const call = peer.call(userId, stream);
-    const video = document.createElement("video");
-    call.on("stream", (userVideoStream) => {
-        addVideoStream(video, userVideoStream);
-    });
-};
-
-function addVideoStream(video, stream) {
-    video.srcObject = stream;
-    video.addEventListener("loadedmetadata", () => {
-        video.play();
-        $("#video_grid").append(video)
-    });
-};
 
 $(function () {
     $("#show_chat").click(function () {
@@ -77,41 +54,12 @@ $(function () {
         }
     })
 
-    $("#mute_button").click(function () {
-        const enabled = myStream.getAudioTracks()[0].enabled;
-        if (enabled) {
-            myStream.getAudioTracks()[0].enabled = false;
-            html = `<i class="fas fa-microphone-slash"></i>`;
-            $("#mute_button").toggleClass("background_red");
-            $("#mute_button").html(html)
-        } else {
-            myStream.getAudioTracks()[0].enabled = true;
-            html = `<i class="fas fa-microphone"></i>`;
-            $("#mute_button").toggleClass("background_red");
-            $("#mute_button").html(html)
-        }
-    })
+   
 
-    $("#stop_video").click(function () {
-        const enabled = myStream.getVideoTracks()[0].enabled;
-        if (enabled) {
-            myStream.getVideoTracks()[0].enabled = false;
-            html = `<i class="fas fa-video-slash"></i>`;
-            $("#stop_video").toggleClass("background_red");
-            $("#stop_video").html(html)
-        } else {
-            myStream.getVideoTracks()[0].enabled = true;
-            html = `<i class="fas fa-video"></i>`;
-            $("#stop_video").toggleClass("background_red");
-            $("#stop_video").html(html)
-        }
-    })
+  
 
 })
 
-peer.on("open", (id) => {
-    socket.emit("join-room", ROOM_ID, id, user);
-});
 
 socket.on("createMessage", (message, userName) => {
     $(".messages").append(`
